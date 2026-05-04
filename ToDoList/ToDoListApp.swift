9d7665cc9e994011b26584738802fdfedd2743c0
Samuel <samuelherbin@orange.fr>
2026-05-04 15:39:14 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Samuel Herbin on 04/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
